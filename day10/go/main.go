46ad63f68a96cadd9c3b155e59464d6061317546
Braydon Kains <kainsbraydon@gmail.com>
2020-12-16 23:36:35 -0500

    did part 1 but too tired

--- day10/go/main.go
+++ day10/go/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"bufio"
-	"math"
+	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func main() {
 	adapters := readAdapters()
-	solve(adapters)
+	sort.Ints(adapters)
+	phoneJolt := getPhoneJoltage(adapters)
+	solve(adapters, phoneJolt)
+	solvePart2(adapters, phoneJolt)
 }
 
 func readAdapters() []int {
@@ -27,9 +31,30 @@ func readAdapters() []int {
 	return adapters
 }
 
-func solve(adapters []int) {
-	currJolt := 0
-	phoneJolt := getPhoneJoltage(adapters)
+func solve(adapters []int, phoneJolt int) {
+	var lastJoltage, onesDiff, threesDiff int
+	for _, adapter := range adapters {
+		diff := adapter - lastJoltage
+		if diff == 1 {
+			onesDiff++
+		} else if diff == 3 {
+			threesDiff++
+		}
+		lastJoltage = adapter
+	}
+	diff := phoneJolt - lastJoltage
+	if diff == 1 {
+		onesDiff++
+	} else if diff == 3 {
+		threesDiff++
+	}
+
+	fmt.Println(onesDiff * threesDiff)
+}
+
+func solvePart2(adapters []int, phoneJolt int) {
+	adjList := make(map[int][]int)
+	adjList[0] = make([]int, 0)
 
 }
 
